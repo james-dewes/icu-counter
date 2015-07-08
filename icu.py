e2afc26bc4e7066afcce88a04b142112e448c6b4
James Dewes <james@jamesdewes.com>
2015-07-08 17:22:44 +0100

    Adding communictaion for Google Analytics

--- icu.py
+++ icu.py
@@ -1,27 +1,53 @@
 import RPi.GPIO as GPIO
-import time
+import time, urllib, urllib2
+
+# get the GA property string
+ga_string = open('prop.txt').read()
+
 GPIO.setmode(GPIO.BCM)
 sensor = 21
-GPIO.setup(sensor,GPIO.IN)
+GPIO.setup(sensor, GPIO.IN)
 
 import RPi.GPIO as GPIO2
 import time
+
 GPIO2.setmode(GPIO.BCM)
 led = 5
-GPIO2.setup(led,GPIO.OUT)
+GPIO2.setup(led, GPIO.OUT)
+
+
+def tell_google(ga_string):
+    """
+    string->
+    :rtype : bool
+    :return: true
+    takes the GA property ID i.e. UA-XXXX-Y and makes a POST request to Google to log activity against
+    that property under the category icu-counter
+    """
+    values = urllib.urlencode(dict(v='1', tid=ga_string, cid='15828', t='Sevent', ec='icu-counter', ea='person'))
+    url = 'http://www.google-analytics.com/collect'
+    headers = {'Content-type': 'application/x-www-form-urlencoded',
+               'Accept': 'text/plain'}
+    headers = ('User-agent':'ICU pepople counter', 'From:':'james@jamesdewes.com')
+    req = urllib2.Request(url, values, headers)
+    response = urllib2.urlopen(req)
+    print(response.geturl())
+    print(response.info())
+    return true
+
 
 test = 0
 while test != 1:
     if GPIO.input(sensor):
         print("I see you")
-        GPIO2.output(led,1)
+        tell_google()
+        GPIO2.output(led, 1)
         time.sleep(2)
-        GPIO2.output(led,0)
+        GPIO2.output(led, 0)
         time.sleep(2)
-        #test = 1
+        # test = 1
     else:
-        #print("waiting")
+        # print("waiting")
         time.sleep(0.5)
-
 
 GPIO.cleanup()
